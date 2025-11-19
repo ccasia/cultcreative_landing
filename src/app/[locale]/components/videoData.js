36f9f8f5f2a37c74d07f90104fd357802730e4ea
sam <shenqjye93@gmail.com>
2025-11-19 10:43:14 +0800

    added card tags and completed video data

--- src/app/[locale]/components/videoData.js
+++ src/app/[locale]/components/videoData.js
@@ -2,154 +2,177 @@ export const videoGallery = [
 	// Row 1
 	{
 		id: 1,
-		thumbnail: "/images/NewCreator/testimonialAvatar2.png",
+		thumbnail:
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/images/thumbnails/Thong%20Jean",
 		videoUrl:
-			"https://storage.googleapis.com/landing-cultcreative/brands/cm7vxwa0j014tnu01f6ybqqan_draft%20(1).mp4",
-		alt: "Woman with makeup",
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/videos/ThongJean.mp4",
+		alt: "Move DPC - Thong Jean",
 	},
 	{
 		id: 2,
-		thumbnail: "/images/NewCreator/testimonialAvatar1.png",
+		thumbnail:
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/images/thumbnails/Mamee%20-%20Adam%20Hazly.png",
 		videoUrl:
-			"https://storage.googleapis.com/landing-cultcreative/brands/cm96oy47r02hcqe01mfl7ehsp_copy_3235B186-45B2-4061-B1F5-9D0E2AB93812%20(1).mp4",
-
-		alt: "Beauty Product",
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/videos/mamee.mp4",
+		alt: "Mamee - Adam Hazly",
 	},
 	{
 		id: 3,
-		thumbnail: "/images/NewCreator/testimonialAvatar2.png",
+		thumbnail:
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/images/thumbnails/Silkygirl%20.png",
 		videoUrl:
-			"https://storage.googleapis.com/landing-cultcreative/brands/cm7vxwa0j014tnu01f6ybqqan_draft%20(1).mp4",
-		alt: "Woman in white dress",
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/videos/SilkyGirl.mp4",
+		alt: "Silkygirl",
 	},
 	{
+		//dummy id
 		id: 4,
-		thumbnail: "/images/NewCreator/testimonialAvatar1.png",
+		thumbnail:
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/images/thumbnails/Strepsils%20-%20Shannie%3AWei%20Shan.png",
 		videoUrl:
-			"https://storage.googleapis.com/landing-cultcreative/brands/cm96oy47r02hcqe01mfl7ehsp_copy_3235B186-45B2-4061-B1F5-9D0E2AB93812%20(1).mp4",
-
-		alt: "Black shoes",
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/videos/Strepsils.mp4",
+		alt: "Strepsils - Shannie/Wei Shan",
 	},
 	{
 		id: 5,
-		thumbnail: "/images/NewCreator/testimonialAvatar2.png",
+		thumbnail:
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/images/thumbnails/Strepsils%20-%20Shannie%3AWei%20Shan.png",
 		videoUrl:
-			"https://storage.googleapis.com/landing-cultcreative/brands/cm7vxwa0j014tnu01f6ybqqan_draft%20(1).mp4",
-		alt: "Boat on the water",
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/videos/Strepsils.mp4",
+		alt: "Strepsils - Shannie/Wei Shan",
 	},
 	{
 		id: 6,
-		thumbnail: "/images/NewCreator/testimonialAvatar1.png",
+		thumbnail:
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/images/thumbnails/Cult's%20Creator%20Passport%20Campaign%20-%20Sabrina%20Haidz.png",
 		videoUrl:
-			"https://storage.googleapis.com/landing-cultcreative/brands/cm96oy47r02hcqe01mfl7ehsp_copy_3235B186-45B2-4061-B1F5-9D0E2AB93812%20(1).mp4",
-		alt: "White sneakers",
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/videos/SabrinaHaidz.mp4",
+		alt: "Cult's Creator Passport Campaign - Sabrina Haidz",
 	},
 	// Row 2
 	{
+		//pending atiq
 		id: 7,
-		thumbnail: "/images/NewCreator/testimonialAvatar2.png",
+		thumbnail:
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/images/thumbnails/jannah.png",
 		videoUrl:
-			"https://storage.googleapis.com/landing-cultcreative/brands/cm7vxwa0j014tnu01f6ybqqan_draft%20(1).mp4",
-		alt: "Making coffee",
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/videos/Gaviscon%20-%20Jannah.mp4",
+		alt: "Gavison - Jannah",
 	},
 	{
+		//pending atiq
 		id: 8,
-		thumbnail: "/images/NewCreator/testimonialAvatar1.png",
+		thumbnail:
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/images/thumbnails/nai.png",
 		videoUrl:
-			"https://storage.googleapis.com/landing-cultcreative/brands/cm96oy47r02hcqe01mfl7ehsp_copy_3235B186-45B2-4061-B1F5-9D0E2AB93812%20(1).mp4",
-		alt: "Skincare product",
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/videos/Presence%20-%20Nai.mov",
+		alt: "Presence - Nai",
 	},
 	{
 		id: 9,
-		thumbnail: "/images/NewCreator/testimonialAvatar2.png",
+		thumbnail:
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/images/thumbnails/Wah%20Chan%20-%20Kristen%20ALyssa%20Sylvanmani.png",
 		videoUrl:
-			"https://storage.googleapis.com/landing-cultcreative/brands/cm7vxwa0j014tnu01f6ybqqan_draft%20(1).mp4",
-		alt: "Woman smiling",
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/videos/KristenAlyssa.mp4",
+		alt: "Wah Chan - Kristen ALyssa Sylvanmani",
 	},
 	{
 		id: 10,
-		thumbnail: "/images/NewCreator/testimonialAvatar1.png",
+		thumbnail:
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/images/thumbnails/Dressing%20Paula%201.png",
 		videoUrl:
-			"https://storage.googleapis.com/landing-cultcreative/brands/cm96oy47r02hcqe01mfl7ehsp_copy_3235B186-45B2-4061-B1F5-9D0E2AB93812%20(1).mp4",
-		alt: "Folding clothes",
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/videos/DressingPaula1.mp4",
+		alt: "Dressing Paula",
 	},
 	{
+		// dummy id
 		id: 11,
-		thumbnail: "/images/NewCreator/testimonialAvatar2.png",
+		thumbnail:
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/images/thumbnails/Dressing%20Paula%203.png",
 		videoUrl:
-			"https://storage.googleapis.com/landing-cultcreative/brands/cm96oy47r02hcqe01mfl7ehsp_copy_3235B186-45B2-4061-B1F5-9D0E2AB93812%20(1).mp4",
-
-		alt: "Woman in yellow sweater",
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/videos/DressingPaula3.mp4",
+		alt: "Dressing Paula",
 	},
 	{
 		id: 12,
-		thumbnail: "/images/NewCreator/testimonialAvatar2.png",
+		thumbnail:
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/images/thumbnails/Dressing%20Paula%203.png",
 		videoUrl:
-			"https://storage.googleapis.com/landing-cultcreative/brands/cm7vxwa0j014tnu01f6ybqqan_draft%20(1).mp4",
-		alt: "Woman posing",
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/videos/DressingPaula3.mp4",
+		alt: "Dressing Paula",
 	},
 	// Row 3
 	{
 		id: 13,
-		thumbnail: "/images/NewCreator/testimonialAvatar2.png",
+		thumbnail:
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/images/thumbnails/Semua%20House%20-%20Venice.png",
 		videoUrl:
-			"https://storage.googleapis.com/landing-cultcreative/brands/cm96oy47r02hcqe01mfl7ehsp_copy_3235B186-45B2-4061-B1F5-9D0E2AB93812%20(1).mp4",
-		alt: "Woman posing",
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/videos/Venice.mp4",
+		alt: "Semua House - Venice",
 	},
 	{
 		id: 14,
-		thumbnail: "/images/NewCreator/testimonialAvatar1.png",
+		thumbnail:
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/images/thumbnails/Sundown%20Festival%20-%20Ed.png",
 		videoUrl:
-			"https://storage.googleapis.com/landing-cultcreative/brands/cm7vxwa0j014tnu01f6ybqqan_draft%20(1).mp4",
-		alt: "Woman posing",
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/videos/Ed.mp4",
+		alt: "Sundown Festival - Ed",
 	},
 	{
 		id: 15,
-		thumbnail: "/images/NewCreator/testimonialAvatar2.png",
+		thumbnail:
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/images/thumbnails/Hotel%20Indigo%20Raya%20Buffet%20-%20Sauban.png",
 		videoUrl:
-			"https://storage.googleapis.com/landing-cultcreative/brands/cm7vxwa0j014tnu01f6ybqqan_draft%20(1).mp4",
-		alt: "Woman posing",
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/videos/Sauban.mp4",
+		alt: "Hotel Indigo Raya Buffet - Sauban",
 	},
 	{
 		id: 16,
-		thumbnail: "/images/NewCreator/testimonialAvatar1.png",
+		thumbnail:
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/images/thumbnails/Dressing%20Paula3.png",
 		videoUrl:
-			"https://storage.googleapis.com/landing-cultcreative/brands/cm96oy47r02hcqe01mfl7ehsp_copy_3235B186-45B2-4061-B1F5-9D0E2AB93812%20(1).mp4",
-		alt: "Woman posing",
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/videos/Meet%20Me%20at%20The%20Cafe%20with%20DP.mov",
+		alt: "Dressing Paula",
 	},
 	{
 		id: 17,
-		thumbnail: "/images/NewCreator/testimonialAvatar2.png",
+		thumbnail:
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/images/thumbnails/Barkath%20on%20Tour%20-%20Maclaud%20.png",
 		videoUrl:
-			"https://storage.googleapis.com/landing-cultcreative/brands/cm7vxwa0j014tnu01f6ybqqan_draft%20(1).mp4",
-		alt: "Woman posing",
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/videos/Mclaud.mp4",
+		alt: "Barkath on Tour - Maclaud",
 	},
 	{
+		// dummy id
 		id: 18,
-		thumbnail: "/images/NewCreator/testimonialAvatar1.png",
+		thumbnail:
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/images/thumbnails/Kha%20-%20Venice.png",
 		videoUrl:
-			"https://storage.googleapis.com/landing-cultcreative/brands/cm96oy47r02hcqe01mfl7ehsp_copy_3235B186-45B2-4061-B1F5-9D0E2AB93812%20(1).mp4",
-		alt: "Woman posing",
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/videos/Kha.mp4",
+		alt: "Kha - Venice",
 	},
 	{
 		id: 19,
-		thumbnail: "/images/NewCreator/testimonialAvatar1.png",
+		thumbnail:
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/images/thumbnails/Kha%20-%20Venice.png",
 		videoUrl:
-			"https://storage.googleapis.com/landing-cultcreative/brands/cm96oy47r02hcqe01mfl7ehsp_copy_3235B186-45B2-4061-B1F5-9D0E2AB93812%20(1).mp4",
-		alt: "Woman posing",
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/videos/Kha.mp4",
+		alt: "Kha - Venice",
 	},
 	{
 		id: 20,
-		thumbnail: "/images/NewCreator/testimonialAvatar1.png",
+		thumbnail:
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/images/thumbnails/Yum%20Yard%20-%20Sophia%20.png",
 		videoUrl:
-			"https://storage.googleapis.com/landing-cultcreative/brands/cm96oy47r02hcqe01mfl7ehsp_copy_3235B186-45B2-4061-B1F5-9D0E2AB93812%20(1).mp4",
-		alt: "Woman posing",
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/videos/Sophia.mp4",
+		alt: "Yum Yard - Sophia",
 	},
 	{
 		id: 21,
-		thumbnail: "/images/NewCreator/testimonialAvatar1.png",
+		thumbnail:
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/images/thumbnails/Quay%20-%20Alisa%20Mahzan.png",
 		videoUrl:
-			"https://storage.googleapis.com/landing-cultcreative/brands/cm96oy47r02hcqe01mfl7ehsp_copy_3235B186-45B2-4061-B1F5-9D0E2AB93812%20(1).mp4",
-		alt: "Woman posing",
+			"https://storage.googleapis.com/landing-cultcreative/creator/new/videos/AlizaMahzan.mp4",
+		alt: "Quay - Alisa Mahzan",
 	},
 ];
